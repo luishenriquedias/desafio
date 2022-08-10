7583070a43d91146fcd0ec89224e999952c0fb7b
LUIS\luishenrique <luishenriquedias1869@gmail.com>
2022-08-10 10:08:06 -0300

    front sem responsividade

---
+++ js/funcoes.js
@@ -0,0 +1,40 @@
+function fecharModal(){
+    let cortina = document.querySelector('#cortina');
+    let modal = document.querySelector('#modal');
+
+    modal.style.background = "none";
+    cortina.style.display = 'none';
+
+}
+
+function abreModalForm(){
+    let cortinaForm = document.querySelector('#cortina-form');
+    let modalForm = document.querySelector('#modal-form');
+
+    cortinaForm.style.display = 'block';
+    modalForm.style.display = 'block';
+}
+
+function fechaModalForm(){
+    let cortinaForm = document.querySelector('#cortina-form');
+    let modalForm = document.querySelector('#modal-form');
+
+    cortinaForm.style.display = 'none';
+    modalForm.style.display = 'none';
+}
+
+function fechaModalLogin(){
+    let cortinaLogin = document.querySelector('#cortina-login');
+    let modalLogin = document.querySelector('#modal-login');
+
+    cortinaLogin.style.display = 'none';
+    modalLogin.style.display = 'none';
+}
+
+function abreModalLogin(){
+    let cortinaLogin = document.querySelector('#cortina-login');
+    let modalLogin = document.querySelector('#modal-login');
+
+    cortinaLogin.style.display = 'block';
+    modalLogin.style.display = 'block';
+}
